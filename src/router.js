ce37adb69274a67a754f617483212e9d2f7b7e53
iqbal <emixbal@gmail.com>
2022-11-07 18:06:34 +0700

    init error page

--- src/router.js
+++ src/router.js
@@ -1,42 +1,55 @@
 import Home from "./pages/Home"
 import AuthLogin from "./pages/AuthLogin"
 import AuthRegister from "./pages/AuthRegister"
+import ErrorPage from "./pages/error-page";
 
 export const myRouters = [
     {
-        label: "Home",
-        path: "",
-        element: <Home />,
-        children: "",
-    },
-    {
-        label: "Home2",
-        path: "/home2",
-        element: <Home />,
-        children: "",
-    },
-    {
-        label: "Users",
-        path: "users",
-        element: <Home />,
+        path: "/",
+        errorElement: <ErrorPage />,
         children: [
             {
-                label: "Menu Users 1",
-                path: "users_menu_1",
+                label: "Home",
+                path: "",
                 element: <Home />,
+                children: "",
             },
             {
-                label: "Menu Users 2",
-                path: "users_menu_2",
+                label: "Home",
+                path: "home",
                 element: <Home />,
+                children: "",
             },
-        ],
-    },
-    {
-        label: "Team",
-        path: "team",
-        element: <Home />,
-        children: "",
+            {
+                label: "Home2",
+                path: "/home2",
+                element: <Home />,
+                children: "",
+            },
+            {
+                label: "Users",
+                path: "users",
+                element: <Home />,
+                children: [
+                    {
+                        label: "Menu Users 1",
+                        path: "users_menu_1",
+                        element: <Home />,
+                    },
+                    {
+                        label: "Menu Users 2",
+                        path: "users_menu_2",
+                        element: <Home />,
+                    },
+                ],
+            },
+            {
+                label: "Team",
+                path: "team",
+                element: <Home />,
+                children: "",
+            }
+        ]
     },
     {
         path: "Login",
@@ -47,10 +60,10 @@ export const myRouters = [
         path: "Register",
         element: <AuthRegister />,
         children: "",
-    },
+    }
 ];
 
 
 export const myAuthRouters = [
-    
+
 ]
